d1c93c0ad33b8c4c6eec353635209ccecb65da00
Yohann THEPAUT <contact@ythepaut.com>
2021-06-23 19:05:45 +0200

    Improved toxicity detection and reports

--- src/helper/ToxicityAnalysis.ts
+++ src/helper/ToxicityAnalysis.ts
@@ -1,26 +1,51 @@
 import config from "../config.json";
 
-async function getToxicity(content: String): Promise<number> {
+interface ToxicityAttribute {
+    name: string;
+    value: number;
+}
 
-    const Perspective = require('perspective-api-client');
+const attributes = [
+    "TOXICITY",
+    "SEVERE_TOXICITY",
+    "IDENTITY_ATTACK_EXPERIMENTAL",
+    "INSULT_EXPERIMENTAL",
+    "PROFANITY_EXPERIMENTAL",
+    "THREAT_EXPERIMENTAL"
+];
+
+async function getToxicity(content: String): Promise<object> {
+
+    const Perspective = require("perspective-api-client");
     const perspective = new Perspective({
         apiKey: config.PERSPECTIVE_API_TOKEN
     });
 
-    let result = null;
+    let rawResult = null;
 
     try {
-        result = await perspective.analyze(
+        rawResult = await perspective.analyze(
             content,
             {
-                attributes: ["toxicity"],
-                doNotStore: true
+                attributes: attributes,
+                doNotStore: true,
+                languages: ["fr"]
             }
         );
-    } catch (_) {
+    } catch (err) {
+        if (config.VERBOSE_LEVEL >= 4)
+            console.error(err);
     }
 
-    return result ? result.attributeScores.TOXICITY.summaryScore.value : -1;
+
+    if (rawResult === null) {
+        return [];
+    } else {
+        let result: any = {};
+        for (const attribute of attributes)
+            result[attribute] = rawResult.attributeScores[attribute].summaryScore.value;
+        return result;
+    }
 }
 
 export default getToxicity;
