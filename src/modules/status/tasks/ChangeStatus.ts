76b1974cd3ae9c781dca234d84ddf20b6a443e29
Yohann THEPAUT <contact@ythepaut.com>
2021-08-05 21:12:57 +0200

    Added bot status

---
+++ src/modules/status/tasks/ChangeStatus.ts
@@ -0,0 +1,27 @@
+import Task from "../../../model/Task";
+import {getClient} from "../../../app";
+import config from "../../../config.json";
+import {ClientUser} from "discord.js";
+
+export class ChangeStatus extends Task {
+
+    private index: number = 0;
+    private statuses = [
+        "McArcades",
+        "PLAY.MCARCADES.FR"
+    ];
+
+    constructor() {
+        super();
+        setInterval(() => this.tick(), 10000);
+    }
+
+    protected tick(): void {
+        getClient().guilds.cache.get(config.GUILD)?.member(getClient().user as ClientUser)?.setNickname("Bob");
+        getClient().user?.setActivity(
+            this.statuses[this.index++],
+            {type: "PLAYING"}
+        );
+    }
+
+}
