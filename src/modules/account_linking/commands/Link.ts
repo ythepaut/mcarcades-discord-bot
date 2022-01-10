46af11b33ad02633fefa1b929385392b4de9f803
Yohann THEPAUT <contact@ythepaut.com>
2022-01-10 11:53:14 +0100

    Add warning message on !link if already linked

--- src/modules/account_linking/commands/Link.ts
+++ src/modules/account_linking/commands/Link.ts
@@ -1,9 +1,10 @@
 import {Command, CommandMessage, Guard} from "@typeit/discord";
 import {NotBot} from "../../../guards/NotBot";
-import {HasRoles} from "../../../guards/HasRoles";
-import config from "../../../config.json";
 import {MessageEmbed} from "discord.js";
 import {getSaltedHash} from "../../../utils/Crypto";
+import Member from "../../../model/database/Member";
+import {getDatabase} from "../../../app";
+import getMinecraftUser from "../../../utils/MinecraftUserResolver";
 
 export abstract class Link {
 
@@ -12,14 +13,29 @@ export abstract class Link {
     private async processCommand(command: CommandMessage) {
 
         const dm = await command.author.createDM();
-        await dm.send(new MessageEmbed()
-            .setColor("#9b59b6")
-            .setTitle("Lier le compte minecraft")
-            .setDescription(
-                "Pour lier votre compte minecraft avec votre compte discord, connectez-vous sur le serveur et tapez la commande :\n" +
-                `\`/discord ${command.author.tag} ${getSaltedHash(command.author.tag).substring(0, 8)}\``)
-            .setFooter("Récompense : 5000 Jetons et 5000 XP")
-        );
+
+        const member = await Member.getFromDid(command.author.id, getDatabase());
+
+        if (member.getUUID() !== null) {
+            const mcUser = await getMinecraftUser(member.getUUID() as string);
+            await dm.send(new MessageEmbed()
+                .setColor("#f1c40f")
+                .setTitle("Déjà lié")
+                .setDescription(
+                    "Votre compte Discord est déjà lié au compte Minecraft " +
+                    `**${mcUser.username}**`)
+                .setFooter("S'il s'agit d'une erreur, contactez un administrateur en ouvrant un ticket.")
+            );
+        } else {
+            await dm.send(new MessageEmbed()
+                .setColor("#9b59b6")
+                .setTitle("Lier le compte minecraft")
+                .setDescription(
+                    "Pour lier votre compte minecraft avec votre compte discord, connectez-vous sur le serveur et tapez la commande :\n" +
+                    `\`/discord ${command.author.tag} ${getSaltedHash(command.author.tag).substring(0, 8)}\``)
+                .setFooter("Récompense : 5000 Jetons et 5000 XP")
+            );
+        }
 
     }
 }
